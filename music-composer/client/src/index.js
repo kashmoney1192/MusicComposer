16151e7daa55467b16bb947f4d5fadbc33f6662f
AiGames <kashmoney571192@gmail.com>
2025-10-21 20:36:25 -0400

    Use HashRouter for GitHub Pages routing

--- music-composer/client/src/index.js
+++ music-composer/client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+  <HashRouter basename="/MusicComposer">
       <AuthProvider>
         <App />
         <ToastContainer
@@ -28,6 +28,6 @@ root.render(
           className="mt-16"
         />
       </AuthProvider>
-    </BrowserRouter>
+  </HashRouter>
   </React.StrictMode>
 );
